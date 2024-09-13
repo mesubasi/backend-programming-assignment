56f345ab5330e6e9ee8235bc8e3b81f65d8bdae4
Muhammed Emin Subaşı <mesub08@gmail.com>
2024-09-13 22:55:10 +0300

    Added Edits

--- controllers/queryController.js
+++ controllers/queryController.js
@@ -1,1 +1,8 @@
 // queryController.js
+
+const handleQuery = async (req, res) => {
+  try {
+  } catch (err) {
+    console.log(err);
+  }
+};
--- routes/query.js
+++ routes/query.js
@@ -4,6 +4,6 @@ const express = require("express");
 const router = express.Router();
 const queryController = require("../controllers/queryController");
 
-router.get("/:datePrefix");
+router.get("/:datePrefix", queryController.handleQuery);
 
 module.exports = router;
