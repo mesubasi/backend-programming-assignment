8c4102efd3eb1919f4897a15d9df6d2a6fd96c7d
Muhammed Emin Subaşı <mesub08@gmail.com>
2024-09-13 22:59:50 +0300

    Edits .gitignore

--- routes/query.js
+++ routes/query.js
@@ -4,6 +4,6 @@ const express = require("express");
 const router = express.Router();
 const queryController = require("../controllers/queryController");
 
-router.get("/:datePrefix", queryController.handleQuery);
+router.get("/:dateprefix", queryController.handleQuery);
 
 module.exports = router;
