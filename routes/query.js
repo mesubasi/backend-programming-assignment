07c3a4d12651b11a6f1f8a86af2ba2dd657b7ab7
Muhammed Emin Subaşı <mesub08@gmail.com>
2024-09-13 22:51:30 +0300

    Added for new edits

--- routes/query.js
+++ routes/query.js
@@ -4,6 +4,6 @@ const express = require("express");
 const router = express.Router();
 const queryController = require("../controllers/queryController");
 
-router.get("/queries/count/:datePrefix");
+router.get("/:datePrefix");
 
 module.exports = router;
--- server.js
+++ server.js
@@ -4,9 +4,12 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
-});
+// routes
+const queryRoute = require("./routes/query");
+
+//middlewares
+app.use(express.json());
+app.use("/queries/count", queryRoute);
 
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
