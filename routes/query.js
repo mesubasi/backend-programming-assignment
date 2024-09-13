c51ec9c868a5d9438f00d2b48576e2d93e23f908
Muhammed Emin Subaşı <mesub08@gmail.com>
2024-09-13 22:47:30 +0300

    Add router

--- routes/query.js
+++ routes/query.js
@@ -1,1 +1,7 @@
 // query.js
+
+const express = require("express");
+const router = express.Router();
+const queryController = require("../controllers/queryController");
+
+router.get("/queries/count/:datePrefix");
