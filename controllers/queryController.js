2de61c09154d5695383d8ce348385ac602aa0ad4
Muhammed Emin Subaşı <mesub08@gmail.com>
2024-09-14 17:39:51 +0300

    Added fs and path module in node

--- controllers/queryController.js
+++ controllers/queryController.js
@@ -1,13 +1,15 @@
 // queryController.js
+
 const path = require("path");
-const fs = require("fs");
+const fs = require("fs").promises;
 
 const handleQuery = async (req, res) => {
   try {
     const logFile = path.join(__dirname, "..", "middleware", "hn_logs.tsv");
-    console.log(logFile);
+    const logData = await fs.readFile(logFile, "utf-8");
   } catch (err) {
     console.log(err);
+    res.status(500).json({ err: "Error" });
   }
 };
 
