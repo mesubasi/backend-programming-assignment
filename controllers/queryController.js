b751dc0a22bc751488573f4076b8ff24a8f33abf
Muhammed Emin Subaşı <mesub08@gmail.com>
2024-09-14 20:23:56 +0300

    Split Procs Added

--- controllers/queryController.js
+++ controllers/queryController.js
@@ -1,15 +1,20 @@
 // queryController.js
 
 const path = require("path");
-const fs = require("fs").promises;
+const fs = require("fs");
 
 const handleQuery = async (req, res) => {
   try {
-    const logFile = path.join(__dirname, "..", "middleware", "hn_logs.tsv");
-    const logData = await fs.readFile(logFile, "utf-8");
+    const { dateprefix } = req.params;
+    const logFile = path.join(__dirname, "..", "middleware", "test.tsv");
+    fs.readFile(logFile, "utf-8", (err, data) => {
+      if (err) throw err;
+      const dataLine = data.split("\n");
+      const dataTab = dataLine.map((line) => line.split("\t"));
+      res.status(200).json({ dataTab });
+    });
   } catch (err) {
-    console.log(err);
-    res.status(500).json({ err: "Error" });
+    res.status(500).json({ error: "Error", details: err.message });
   }
 };
 
