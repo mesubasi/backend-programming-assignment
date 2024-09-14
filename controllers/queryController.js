8cfa3794f12b1914a7ee2b201f61940a57436445
Muhammed Emin Subaşı <mesub08@gmail.com>
2024-09-15 00:23:05 +0300

    Test Success

--- controllers/queryController.js
+++ controllers/queryController.js
@@ -1,18 +1,23 @@
 // queryController.js
 
 const path = require("path");
-const fs = require("fs");
+const fs = require("fs").promises;
 
 const handleQuery = async (req, res) => {
   try {
     const { dateprefix } = req.params;
-    const logFile = path.join(__dirname, "..", "middleware", "test.tsv");
-    fs.readFile(logFile, "utf-8", (err, data) => {
-      if (err) throw err;
-      const dataLine = data.split("\n");
-      const dataTab = dataLine.map((line) => line.split("\t"));
-      res.status(200).json({ dataTab });
-    });
+    const logFile = path.join(__dirname, "..", "middleware", "hn_logs.tsv");
+    const data = await fs.readFile(logFile, "utf-8");
+    const dataLines = data.split("\n");
+
+    const dataTab = dataLines
+      .map((line) => line.split("\t"))
+      .filter((dataLine) => dataLine[0].startsWith(dateprefix));
+
+    const date = dataTab.map((item) => item[0]);
+    const dateLength = date.length;
+
+    res.status(200).json({ count: dateLength });
   } catch (err) {
     res.status(500).json({ error: "Error", details: err.message });
   }
